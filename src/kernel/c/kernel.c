e02baf776b985abc1967e74dd3b42a67c8ce61d3
atpur-rafif <muhammadatpurrafif@gmail.com>
2024-03-10 08:02:25 +0700

    Remove unused code

--- src/kernel/c/kernel.c
+++ src/kernel/c/kernel.c
@@ -9,14 +9,6 @@
 #include <std/stdint.h>
 #include <std/string.h>
 
-void syscalln(uint32_t eax, uint32_t ebx, uint32_t ecx, uint32_t edx) {
-	__asm__ volatile("mov %0, %%ebx" : /* <Empty> */ : "r"(ebx));
-	__asm__ volatile("mov %0, %%ecx" : /* <Empty> */ : "r"(ecx));
-	__asm__ volatile("mov %0, %%edx" : /* <Empty> */ : "r"(edx));
-	__asm__ volatile("mov %0, %%eax" : /* <Empty> */ : "r"(eax));
-	__asm__ volatile("int $0x30");
-}
-
 void kernel_setup(void) {
 	load_gdt(&_gdt_gdtr);
 	pic_remap();
