ad75ecde18ae49ea326f23c9bc1a5092a5b05c27
MRafliRasyiidin <masraflirasid@gmail.com>
2024-03-11 13:42:49 +0700

    feat: added framebuffer (chapter 1.1)

--- src/kernel.c
+++ src/kernel.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include "header/cpu/gdt.h"
 #include "header/kernel-entrypoint.h"
+#include "header/text/framebuffer.h"
+#include "header/cpu/portio.h"
 
 void kernel_setup(void) {
     load_gdt(&_gdt_gdtr);
